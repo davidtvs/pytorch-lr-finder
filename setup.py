01a7b606b7b61100e220801287dbca3c7ac67087
David Silva <davidtvs10@gmail.com>
2020-05-03 22:55:17 +0200

    Test deploy v3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="torch-lr-finder",
-    version="0.1.4",
+    version="0.1.5.1",
     author="David Silva",
     author_email="davidtvs10@gmail.com",
     description="Pytorch implementation of the learning rate range test",
