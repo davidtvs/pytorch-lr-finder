Fix learning rate history and learning rate computation is schedulers (#43, #42)
* Add unit test related to #42

Two things can be concluded:
* The current computation of the exponential learning rate is incorrect in all version of PyTorch above 0.4.1
* PyTorch 1.4.0 introduced a different design for the LRScheduler; the fix for the exponential learning rate for version 0.4.1 is not the same as the one for 1.4.0

Setup for the above:
1. torch==0.4.1 torchvision==0.2.1
2. torch==1.4.0 torchvision==0.5.0

* Fix learning rate computation in schedulers

* Add PyTorch matrix to the CI job

Also, added caching to make the CI job faster.

* Raise ValueError for num_iter<=1

* Fix syntax error in CI yaml

* Fix syntax error in CI yaml v2

* Fix CI job

* The combo of py3.7 and torch 0.4.1 breaks type inference for torch.tensor with np.int64

* Allow CI to be skipped

tests/conftest.py
@@ -1,4 +1,8 @@
 import pytest
+import random
+import os
+import numpy as np
+import torch
 
 
 class CustomCommandLineOption(object):
@@ -37,5 +41,16 @@ def pytest_addoption(parser):
 def pytest_configure(config):
     # Bind a config object to `pytest` module instance
     pytest.custom_cmdopt = CustomCommandLineOption()
-
     pytest.custom_cmdopt.add("cpu_only", config.getoption("--cpu_only"))
+
+    # Set the random seed so that the tests are reproducible between test runs and
+    # hopefully torch and numpy versions. This seed should also allow all range tests
+    # with a starting lr of 1e-5 and an ending lr of 1e-1 to run the full test without
+    # diverging
+    seed = 1
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True

tests/dataset.py
@@ -15,9 +15,9 @@ class XORDataset(Dataset):
         _shape = (length,) + tuple(shape) if shape else (length, 8)
         raw = np.random.randint(0, 2, _shape)
         self.data = torch.FloatTensor(raw)
-        self.label = (
-            torch.tensor(np.bitwise_xor.reduce(raw, axis=1)).unsqueeze(dim=1).float()
-        )
+
+        label = np.bitwise_xor.reduce(raw, axis=1)
+        self.label = torch.tensor(label, dtype=torch.float32).unsqueeze(dim=1)
 
     def __getitem__(self, index):
         return self.data[index], self.label[index]

torch_lr_finder/lr_finder.py
@@ -6,6 +6,9 @@ from torch.optim.lr_scheduler import _LRScheduler
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
+from packaging import version
+
+PYTORCH_VERSION = version.parse(torch.__version__)
 
 try:
     from apex import amp
@@ -161,7 +164,8 @@ class LRFinder(object):
         smooth_f=0.05,
         diverge_th=5,
         accumulation_steps=1,
-        non_blocking_transfer=True):
+        non_blocking_transfer=True
+    ):
         """Performs the learning rate range test.
 
         Arguments:
@@ -291,8 +295,8 @@ class LRFinder(object):
                 )
 
             # Update the learning rate
-            lr_schedule.step()
             self.history["lr"].append(lr_schedule.get_lr()[0])
+            lr_schedule.step()
 
             # Track the best loss and smooth it if smooth_f is specified
             if iteration == 0:
@@ -484,12 +488,23 @@ class LinearLR(_LRScheduler):
 
     def __init__(self, optimizer, end_lr, num_iter, last_epoch=-1):
         self.end_lr = end_lr
+
+        if num_iter <= 1:
+            raise ValueError("`num_iter` must be larger than 1")
         self.num_iter = num_iter
+
         super(LinearLR, self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
-        curr_iter = self.last_epoch + 1
-        r = curr_iter / self.num_iter
+        # In earlier Pytorch versions last_epoch starts at -1, while in recent versions
+        # it starts at 0. We need to adjust the math a bit to handle this. See
+        # discussion at: https://github.com/davidtvs/pytorch-lr-finder/pull/42
+        if PYTORCH_VERSION < version.parse("1.1.0"):
+            curr_iter = self.last_epoch + 1
+            r = curr_iter / (self.num_iter - 1)
+        else:
+            r = self.last_epoch / (self.num_iter - 1)
+
         return [base_lr + r * (self.end_lr - base_lr) for base_lr in self.base_lrs]
 
 
@@ -506,12 +521,23 @@ class ExponentialLR(_LRScheduler):
 
     def __init__(self, optimizer, end_lr, num_iter, last_epoch=-1):
         self.end_lr = end_lr
+
+        if num_iter <= 1:
+            raise ValueError("`num_iter` must be larger than 1")
         self.num_iter = num_iter
+
         super(ExponentialLR, self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
-        curr_iter = self.last_epoch + 1
-        r = curr_iter / self.num_iter
+        # In earlier Pytorch versions last_epoch starts at -1, while in recent versions
+        # it starts at 0. We need to adjust the math a bit to handle this. See
+        # discussion at: https://github.com/davidtvs/pytorch-lr-finder/pull/42
+        if PYTORCH_VERSION < version.parse("1.1.0"):
+            curr_iter = self.last_epoch + 1
+            r = curr_iter / (self.num_iter - 1)
+        else:
+            r = self.last_epoch / (self.num_iter - 1)
+
         return [base_lr * (self.end_lr / base_lr) ** r for base_lr in self.base_lrs]
 
 